de55d840fceb9840cc46b42d6d2f8fb2eaad56a4
Ayoub Ed-dafali <ayoub.eddafali@gmail.com>
2022-04-07 11:14:24 +0000

    Initial commit

---
+++ src/main/java/com/elastisys/javaapp/JavaAppApplication.java
@@ -0,0 +1,126 @@
+package com.elastisys.javaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.compression.JdkZlibDecoder;
+import io.netty.handler.codec.compression.JdkZlibEncoder;
+import org.codehaus.plexus.archiver.zip.ZipArchiver;
+import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
+import com.google.json.JsonSanitizer;
+// import org.codehaus.plexus.logging.console.ConsoleLogger;
+import org.codehaus.plexus.util.FileUtils;
+import java.nio.charset.StandardCharsets;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.thoughtworks.xstream.XStream;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@SpringBootApplication
+@RestController
+public class JavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppApplication.class, args);
+	}
+
+	private static byte[] compress(byte[] data) {
+        JdkZlibEncoder enc = new JdkZlibEncoder();
+        EmbeddedChannel channel = new EmbeddedChannel(enc);
+        channel.writeOutbound(Unpooled.wrappedBuffer(data));
+        channel.finish();
+
+        int outputSize = 0;
+        ByteBuf o;
+        List<ByteBuf> outbound = new ArrayList<>();
+        while ((o = channel.readOutbound()) != null) {
+            outbound.add(o);
+            outputSize += o.readableBytes();
+        }
+
+        byte[] output = new byte[outputSize];
+        int readCount = 0;
+        for (ByteBuf b : outbound) {
+            int readableBytes = b.readableBytes();
+            b.readBytes(output, readCount, readableBytes);
+            b.release();
+            readCount += readableBytes;
+        }
+
+        return output;
+    }
+
+    private static byte[] decompress(byte[] data) {
+        JdkZlibDecoder dec = new JdkZlibDecoder();
+        EmbeddedChannel channel = new EmbeddedChannel(dec);
+        channel.writeInbound(Unpooled.wrappedBuffer(data));
+        channel.finish();
+
+        int outputSize = 0;
+        ByteBuf o;
+        List<ByteBuf> inbound = new ArrayList<>();
+        while ((o = channel.readInbound()) != null) {
+            inbound.add(o);
+            outputSize += o.readableBytes();
+        }
+
+        byte[] output = new byte[outputSize];
+        int readCount = 0;
+        for (ByteBuf b : inbound) {
+            int readableBytes = b.readableBytes();
+            b.readBytes(output, readCount, readableBytes);
+            b.release();
+            readCount += readableBytes;
+        }
+
+        return output;
+    }
+
+
+	@GetMapping("/decompress")
+		public String decompress(@RequestParam(value = "data", defaultValue = "example data") String data) {
+		byte[] original = data.getBytes(StandardCharsets.UTF_8);
+        byte[] compressed = compress(original);
+        byte[] decompressed = decompress(compressed);
+		String output = "Original: " + new String(original) + "\n"; 
+		output += "Compressed: " + new String(compressed) + "\n"; 
+		output += "Decompressed: " + new String(decompressed);
+
+		return output;
+	}
+
+    @GetMapping("/vulnerable")
+        public String sanitize(){
+
+            String jsonString = "{\"key1\":\"value1\",\"type\":\"Booking\",\"sid\":\"A43521\",\"region\":\"ASIA\","
+				+ "\"fetchFromFile\":\"false\",\"service\":\"true\",\"isEom\":\"true\",*#@!}";
+            String response = JsonSanitizer.sanitize(jsonString);
+
+            List <String> list = new ArrayList <String>();
+            list.add("a");
+            list.add("b");
+
+            XStream xStream = new XStream();
+            xStream.alias("Strings", List.class);
+            xStream.alias("String", String.class);
+            String result = xStream.toXML(list);
+            // new Exception("Stack trace").printStackTrace();
+            // new Throwable().printStackTrace();
+            return response + result;
+        }
+
+
+}
---
+++ src/test/java/com/elastisys/javaapp/JavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elastisys.javaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
