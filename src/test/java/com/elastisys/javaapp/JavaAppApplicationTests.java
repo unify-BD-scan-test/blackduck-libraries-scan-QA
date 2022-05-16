5224bc843f4356ea05bc309540798b44b2e7cf3b
ayoubensalem <ayoubensalem@gmail.com>
2022-05-16 11:40:54 +0100

    add initial unit tests

--- src/test/java/com/elastisys/javaapp/JavaAppApplicationTests.java
+++ src/test/java/com/elastisys/javaapp/JavaAppApplicationTests.java
@@ -1,24 +1,28 @@
 package com.elastisys.javaapp;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.Repeat;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class JavaAppApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@BeforeAll
-	public static void init() throws InterruptedException {
-		Thread.sleep(30000);
+	try {
+			Thread.sleep(60000);
+		 } catch (InterruptedException e) {
+		 	throw new RuntimeException(e);
+		 }
 	}
 
 	@Test
+	@RepeatedTest(10)
 	public void testApplication() {
 		JavaAppApplication javaApp = new JavaAppApplication();
 		String result = javaApp.sanitize();
